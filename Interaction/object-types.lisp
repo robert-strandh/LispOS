1184def294678a1c5ddf4888859d78db0cc1e158
Robert Strandh <robert.strandh@gmail.com>
2017-04-15 08:27:51 +0200

    Add more example object types.

--- Interaction/object-types.lisp
+++ Interaction/object-types.lisp
@@ -6,11 +6,32 @@
     :initarg :creation-date
     :reader creation-date)
    (%modification-date
-    :accessor modification-date)))
+    :accessor modification-date)
+   (%author
+    :initarg :author
+    :reader :author)))
 
 (defmethod initialize-instance :after ((object object) &key)
   (setf (modification-date object)
         (creation-date object)))
 
+(defclass sound (object)
+  ())
 
+(defclass music (sound)
+  ((%composer :initarg :composer :reader composer)))
 
+(defclass person (object)
+  ((%name :initarg :name :accessor name)))
+
+(defclass text-document (object)
+  ((%contents :initarg :contents :accessor contents)))
+
+(defclass latex-source (text-document)
+  ())
+
+(defclass email-message (object)
+  ())
+
+(defclass source-code (text-document)
+  ((%language :initarg :language :reader language)))
